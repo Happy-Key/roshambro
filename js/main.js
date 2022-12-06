664a9510f89722120e9bc818abb15617eb0bd8cd
ClownDay <88943993+ClownDay@users.noreply.github.com>
2022-12-05 21:25:27 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function startGame() {
   let inputs = document.getElementsByClassName("rpsInput");
+  document.getElementById('rpsResponse').innerHTML = "Great! Let's play some RoShamBo."
   for (let i = 0; i < inputs.length; i++) {
     inputs[i].style.display = block;
   }

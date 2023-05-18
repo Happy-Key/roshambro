44daddd457eeb1e720a6468dc127d275037cdbe4
ClownDay <88943993+ClownDay@users.noreply.github.com>
2023-05-17 21:05:48 -0700

    Added some rps function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
+const options[] = ["rock", "paper", "scissors", "rock"]
+
 function startGame() {
-  document.getElementById('gameInput').style.display = "none";
-  let inputs = document.getElementsByClassName("rpsInput");
-  document.getElementById('rpsResponse').innerHTML = "Great! Let's play some RoShamBo."
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].style.display = "inline-block";
-  }
+  document.getElementById('gameInitialize').style.display = "none";
+  document.getElementById('rpsInput').style.display = "inline-block";
+  document.getElementById('rpsResponse').style.display = "block";
 }
 
 const endGame = async () => {
@@ -14,3 +13,29 @@ const endGame = async () => {
     await delay(1500);
     location.reload();
   };
+
+function rpsEvaluate(input) {
+  let broInput = getRandomInt(1,3);
+  if (broInput - input == 1) {rpsLoss(broInput); return;}
+  if (broInput - input == 2) {rpsWin(broInput); return;}
+  rpsDraw(broInput);
+  return;
+}
+
+function rpsLoss(broInput) {
+  document.getElementById('rpsResponse').textContent = "You played ${options[broInput-1]} while I played ${options[broInput] which means I win.";
+}
+
+function rpsWin(broInput) {
+  document.getElementById('rpsResponse').textContent = "You played ${options[broInput-1]} while I played ${options[broInput] which means I lose.";
+}
+
+function rpsDraw(broInput) {
+  document.getElementById('rpsResponse').textContent = "You played ${options[broInput-1]} while I played ${options[broInput] which means it's a draw.";
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}

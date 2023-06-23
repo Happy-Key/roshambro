7a232bfa22524fc4c108f7ee897cab53325b20cb
ClownDay <88943993+ClownDay@users.noreply.github.com>
2023-06-23 09:29:45 -0700

    test streaks and stats

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const options = ["rock", "paper", "scissors", "rock"]
 
+var gameData = [
+  totalWins=0,
+  totalLosses=0,
+  totalDraws=0,
+  winstreakMax=0,
+  losestreakMax=0,
+  drawstreakMax=0,
+  currentStreak=0,
+  whatstreak=0,
+];
+
 function startGame() {
   document.getElementById('greeting').style.display = "none";
   document.getElementById('gameInitialize').style.display = "none";
@@ -37,16 +48,41 @@ function rpsEvaluate(input) {
 function rpsLoss(broInput, input) {
   document.getElementById('rpsResponse').textContent = `You played ${options[input]} while I played ${options[broInput]} which means you lose.`;
   document.getElementById('broChat').textContent = "Would you like to play again?";
+  totalLosses += 1;
+  streakCalc(1);
 }
 
 function rpsWin(broInput, input) {
   document.getElementById('rpsResponse').textContent = `You played ${options[input]} but I played ${options[broInput]} which means you win!`;
   document.getElementById('broChat').textContent = "Would you like to play again?";
+  totalWins += 1;
+  streakCalc(0);
 }
 
-function rpsDraw( input) {
+function rpsDraw(input) {
   document.getElementById('rpsResponse').textContent = `We both played ${options[input]} which means it's a draw.`;
   document.getElementById('broChat').textContent = "Would you like to play again?";
+  totalDraws += 1;
+  streakCalc(2);
+}
+
+function streakCalc(latestResult)
+{
+  switch(latestResult) {
+    case 0:
+      if (whatstreak == 0) {currentStreak++;}
+      break
+    case 1:
+      if (whatstreak == 1) {currentStreak++;}
+      break
+    case 2:
+      if (whatstreak == 2) {currentStreak++;}
+      break
+    default:
+      whatstreak = latestResult;
+      currentStreak = 1;
+  }
+  document.getElementById('tempstreakcounter').textContent = `wins:${totalWins}, losses:${totalLosses}, draws:${totalDraws}, streaktype:${whatstreak}, current streak:${currentStreak}`;
 }
 
 function getRandomInt(min, max) {
